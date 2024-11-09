27d221db55ddf666af5257a97f575492c1d25757
Bikaze <clmntmugisha@gmail.com>
2024-11-09 19:42:10 +0200

    Paginate issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,9 +5,10 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import NextLink from "next/link";
 import { Issue, Status } from "@prisma/client";
 import { ArrowUpIcon } from "@radix-ui/react-icons";
+import Pagination from "@/app/components/Pagination";
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue };
+  searchParams: { status: Status; orderBy: keyof Issue; page: string };
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
@@ -20,21 +21,28 @@ const IssuesPage = async ({ searchParams }: Props) => {
   const params = await searchParams;
   const statuses = Object.values(Status);
   const status = statuses.includes(params.status) ? params.status : undefined;
+  const where = { status };
+
   const orderBy = columns.map((column) => column.value).includes(params.orderBy)
     ? { [params.orderBy]: "asc" }
     : undefined;
 
+  const page = parseInt(params.page) || 1;
+  const pageSize = 10;
+
   const issues = await prisma.issue.findMany({
-    where: {
-      status,
-    },
+    where,
     orderBy,
+    skip: (page - 1) * pageSize,
+    take: pageSize,
   });
+
+  const issueCount = await prisma.issue.count({ where });
 
   return (
     <div>
       <IssueActions />
-      <Table.Root variant="surface">
+      <Table.Root variant="surface" mb='3'>
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
@@ -75,6 +83,11 @@ const IssuesPage = async ({ searchParams }: Props) => {
           ))}
         </Table.Body>
       </Table.Root>
+      <Pagination
+        pageSize={pageSize}
+        currentPage={page}
+        itemCount={issueCount}
+      />
     </div>
   );
 };
